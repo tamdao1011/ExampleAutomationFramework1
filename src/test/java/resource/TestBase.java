a08421b2af5c4ff8f762300f5e034bfbcd44cdb3
Tam <daothanhtam88@gmail.com>
2020-03-02 23:00:14 -0600

    Create Screenshot function

--- src/test/java/resource/TestBase.java
+++ src/test/java/resource/TestBase.java
@@ -1,22 +1,46 @@
 package resource;
 
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 
-import static common.BrowserFactory.quitDriver;
+import static common.BrowserFactory.*;
 import static common.CommonFunctions.*;
 
 public class TestBase {
 
     @Parameters({"browser", "url"})
-    @BeforeClass
-    @Step("Navigate to the Home page")
+    @BeforeTest
     public void setup(String browser, String url) {
         setGlobalVariables(browser, url);
+        launchBrowser(browser);
+    }
+
+    @BeforeMethod
+    public void start() {
         navigateToHomePage();
     }
 
-    @AfterSuite
+    @AfterMethod
+    public void clear(ITestResult result) throws Exception {
+        if (result.isSuccess()) {
+            System.out.println("TEST PASSED");
+        } else {
+            System.out.println("TEST FAILED");
+            takeScreenShot(result.getInstance().toString());
+            saveScreenshot(((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES));
+        }
+    }
+
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] saveScreenshot(byte[] screenShot) {
+        return screenShot;
+    }
+
+    @AfterTest
     @Step("Close the browser")
     public void tearDown() {
         quitDriver();
