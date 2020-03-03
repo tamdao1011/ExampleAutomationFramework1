00055e5808344332521ccbef72d82e8e19c0a961
Tam <daothanhtam88@gmail.com>
2020-03-02 22:59:07 -0600

    Edit SIGN_IN_TEXT element

--- src/main/java/pages/SignInPage.java
+++ src/main/java/pages/SignInPage.java
@@ -8,6 +8,6 @@ import static common.CommonFunctions.find;
 public class SignInPage {
 
     //Declare and set value for element variables
-    public static WebElement SIGN_IN_TEXT = find(By.className("a-spacing-small"));
+    public static WebElement SIGN_IN_TEXT = find(By.cssSelector("h1.a-spacing-small"));
 
 }
